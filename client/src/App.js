43d279db65d39e66181c4531a12fe5c725ee5311
Farasat Khan <farasatkahan@gmail.com>
2023-01-12 21:31:06 +0500

    implemented View Individual Student Result and View Material Content

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Link, Route, Routes } from 'react-router-dom';
+
+import { IndividualStudentResult } from './components/IndividualStudentResult';
+import { Materials } from './components/Materials';
+import { NotFound } from './components/NotFound';
 
 function App() {
+
   return (
-    <div>
-      React App Setup
-    </div>
+    <>
+      <ul>
+        <li><Link to='/student_result/63ad59c91ae564e73659044a'>StudentResult</Link></li>
+        <li><Link to='/materials'>Materials</Link></li>
+      </ul>
+      <Routes>
+        <Route path="/student_result/:sid" element={<IndividualStudentResult />}/>
+        <Route path="/materials" element={<Materials />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </>
   )
 }
 
